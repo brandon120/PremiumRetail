590010748e232bff733b4e1162d916b0db99962f
Brandon <brandonjmonroe@gmail.com>
2023-07-14 16:41:53 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'PremiumAdmin120');
-define('DB_NAME', 'premiumretail');
-
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+//The above credentials are blank for protection
 //Creating connection 
 //Using the SQLI method
 $conn = new mysqli(DB_HOST,DB_USER,DB_PASS, DB_NAME);
